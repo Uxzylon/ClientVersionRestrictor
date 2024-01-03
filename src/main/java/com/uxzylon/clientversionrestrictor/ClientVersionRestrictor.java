f7364dfa69ff3bd95184d39d0e1083ab50dc4e1a
anthony.jeanney <anthony.jeanney@gmail.com>
2024-01-03 16:51:46 +0100

    Moved scheduler task in the event

--- src/main/java/com/uxzylon/clientversionrestrictor/ClientVersionRestrictor.java
+++ src/main/java/com/uxzylon/clientversionrestrictor/ClientVersionRestrictor.java
@@ -1,17 +1,11 @@
 package com.uxzylon.clientversionrestrictor;
 
 import com.uxzylon.clientversionrestrictor.Events.ClientConnectionEvent;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public final class ClientVersionRestrictor extends JavaPlugin {
 
     public static ClientVersionRestrictor plugin;
-    public static List<Player> playersToKick = new ArrayList<>();
 
     @Override
     public void onEnable() {
@@ -22,15 +16,6 @@ public final class ClientVersionRestrictor extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new ClientConnectionEvent(), this);
 
         plugin.getLogger().info("Enabled!");
-
-        // Kick outdated players every 5 seconds (needed because kicking on join creates an error)
-        // Also, listening to PlayerLoginEvent doesn't work because ViaVersion returns -1 as the player version
-        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, () -> {
-            for (Player player : playersToKick) {
-                player.kickPlayer(getConfig().getString("kickMessage"));
-            }
-            playersToKick.clear();
-        }, 0L, 100L);
     }
 
     private void createConfig() {
--- src/main/java/com/uxzylon/clientversionrestrictor/Events/ClientConnectionEvent.java
+++ src/main/java/com/uxzylon/clientversionrestrictor/Events/ClientConnectionEvent.java
@@ -9,7 +9,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import static com.uxzylon.clientversionrestrictor.ClientVersionRestrictor.plugin;
-import static com.uxzylon.clientversionrestrictor.ClientVersionRestrictor.playersToKick;
 
 public class ClientConnectionEvent implements Listener {
 
@@ -21,7 +20,8 @@ public class ClientConnectionEvent implements Listener {
         plugin.getLogger().info("Player " + player.getName() + " joined with version " + version);
 
         if (version < ProtocolVersion.v1_14.getVersion() && !player.hasPermission("clientversionrestrictor.bypass")) {
-            playersToKick.add(player);
+            plugin.getServer().getScheduler().runTaskLater(plugin, () ->
+                    player.kickPlayer(plugin.getConfig().getString("kickMessage")), 100L);
         }
     }
 
