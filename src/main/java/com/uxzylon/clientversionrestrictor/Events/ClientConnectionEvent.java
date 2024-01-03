f90c0a5cb3360de8f18ad2e5ed355306f4718809
anthony.jeanney <anthony.jeanney@gmail.com>
2024-01-03 14:59:38 +0100

    Fix console error logs

--- src/main/java/com/uxzylon/clientversionrestrictor/Events/ClientConnectionEvent.java
+++ src/main/java/com/uxzylon/clientversionrestrictor/Events/ClientConnectionEvent.java
@@ -6,15 +6,16 @@ import com.viaversion.viaversion.api.ViaAPI;
 import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
 
 public class ClientConnectionEvent implements Listener {
     @EventHandler
-    public void onJoin(PlayerJoinEvent pje) {
+    public void onPlayerJoin(PlayerLoginEvent event) {
         ViaAPI api = Via.getAPI();
-        int version = api.getPlayerVersion(pje.getPlayer().getUniqueId());
+        int version = api.getPlayerVersion(event.getPlayer().getUniqueId());
         if (version < ProtocolVersion.v1_14.getVersion()) {
-            pje.getPlayer().kickPlayer("You are using an outdated client version. Please update to 1.14 or higher.");
+            event.setKickMessage("You are using an outdated client version. Please update to 1.14 or higher.");
+            event.setResult(PlayerLoginEvent.Result.KICK_OTHER);
         }
     }
 }
