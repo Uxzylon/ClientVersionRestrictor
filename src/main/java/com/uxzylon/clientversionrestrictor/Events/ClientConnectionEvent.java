3dbc75bd65b60b48fb6d370cb73bdd619ab9a489
anthony.jeanney <anthony.jeanney@gmail.com>
2024-01-03 15:08:52 +0100

    Added bypass permission

--- src/main/java/com/uxzylon/clientversionrestrictor/Events/ClientConnectionEvent.java
+++ src/main/java/com/uxzylon/clientversionrestrictor/Events/ClientConnectionEvent.java
@@ -1,9 +1,9 @@
 package com.uxzylon.clientversionrestrictor.Events;
 
 import com.viaversion.viaversion.api.Via;
-import com.viaversion.viaversion.api.ViaAPI;
 
 import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
@@ -11,9 +11,9 @@ import org.bukkit.event.player.PlayerLoginEvent;
 public class ClientConnectionEvent implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerLoginEvent event) {
-        ViaAPI api = Via.getAPI();
-        int version = api.getPlayerVersion(event.getPlayer().getUniqueId());
-        if (version < ProtocolVersion.v1_14.getVersion()) {
+        Player player = event.getPlayer();
+        int version = Via.getAPI().getPlayerVersion(player.getUniqueId());
+        if (version < ProtocolVersion.v1_14.getVersion() && !player.hasPermission("clientversionrestrictor.bypass")) {
             event.setKickMessage("You are using an outdated client version. Please update to 1.14 or higher.");
             event.setResult(PlayerLoginEvent.Result.KICK_OTHER);
         }
